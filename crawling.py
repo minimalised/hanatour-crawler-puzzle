4d0f35987ed138ab415c04be213e49cd320cea4f
minimalised <129501279+minimalised@users.noreply.github.com>
2026-06-05 11:42:51 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -223,7 +223,7 @@ async def run_crawler():
                 "1Hoq0N88mestsHXbIOjwue3OctXf7dvKkx99eieYFhAY",
                 "1BK4xUHQFrLjLTn6vE0jSuwqMvSU7ZMKIV-nPvmySPx8"
             ]
-            worksheet_name = "github_detail"
+            worksheet_name = "github"
 
             try:
                 df = pd.DataFrame(all_products)
