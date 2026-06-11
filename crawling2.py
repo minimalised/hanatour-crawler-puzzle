ea796ebe8dd1254a7a2c48ed9913f6dc79d382b3
minimalised <129501279+minimalised@users.noreply.github.com>
2026-06-11 14:47:50 +0900

    Update crawling2.py

--- crawling2.py
+++ crawling2.py
@@ -159,7 +159,7 @@ async def run_crawler():
     
     try:
         source_doc = gc.open_by_key(source_spreadsheet_id)
-        source_sheet = source_doc.worksheet("상품리스트")
+        source_sheet = source_doc.worksheet("상품리스트2")
     except Exception as e:
         print(f"❌ 소스 스프레드시트 로드 실패: {e}")
         return
